2263cdd623235dfd2bea2eaed5b04342e48dfa72
webcomprojects <webcom.projects@gmail.com>
2024-12-07 15:28:45 +0330

    change ip

--- config/reverb.php
+++ config/reverb.php
@@ -29,7 +29,7 @@ return [
     'servers' => [
 
         'reverb' => [
-            'host' => env('REVERB_SERVER_HOST', '188.40.16.3'),
+            'host' => env('REVERB_SERVER_HOST', '0.0.0.0'),
             'port' => env('REVERB_SERVER_PORT', 8080),
             'hostname' => env('REVERB_HOST'),
             'options' => [
